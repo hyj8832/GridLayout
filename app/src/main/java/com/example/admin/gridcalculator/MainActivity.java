65846851174b4e2f3430545bbc2e6188b68d1e51
hyj8832 <hyj8832@naver.com>
2017-04-06 20:57:01 +0900

    #1 GridCalculator

--- app/src/main/java/com/example/admin/gridcalculator/MainActivity.java
+++ app/src/main/java/com/example/admin/gridcalculator/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.admin.gridcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+import static android.R.id.edit;
+
+public class MainActivity extends AppCompatActivity  {
+    Button[] butNums=new Button[10];
+    Button[] butOps=new Button[4];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for(int i=0; i<butNums.length;i++){
+            butNums[i]=(Button)findViewById(R.id.btn_0+i); //만약에 변수명이 어떤 것은 btn으로 시작하지 않는다면 이렇게 반복문을 돌릴 수 없다.
+            butNums[i].setOnClickListener(butNumHandler);
+        }
+        for(int i=0;i<butOps.length;i++){
+            butOps[i]=(Button)findViewById(R.id.btn_p+i);
+            butOps[i].setOnClickListener(butOpHandler);
+        }
     }
+
+    View.OnClickListener butNumHandler =new View.OnClickListener(){
+
+        public void onClick(View view){
+
+        }
+    };
+
+    View.OnClickListener butOpHandler =new View.OnClickListener(){
+
+        public void onClick(View view){
+
+        }
+    };
 }
